e46d10f5c8b5a033671144ce9b987dca5c0b62a4
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2018-01-28 15:31:01 +0100

    Show crafts

--- app/controllers/crafts_controller.rb
+++ app/controllers/crafts_controller.rb
@@ -7,6 +7,13 @@ class CraftsController < ApplicationController
   end
 
   def create
-    render plain: params[:craft].inspect
+    @craft = Craft.new(params[:craft].permit(:przedmiot, :opis))
+
+    @craft.save
+    redirect_to @craft
+  end
+
+  def show
+    @craft = Craft.find(params[:id])
   end
 end
