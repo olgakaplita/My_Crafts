bd1b381df839304eebe81746faa89743aca868a4
Olga Kaplita <olga@Olgas-MacBook-Pro.local>
2018-02-11 15:25:49 +0100

    Add comment relation and migrate comments

--- app/models/craft.rb
+++ app/models/craft.rb
@@ -1,3 +1,4 @@
 class Craft < ApplicationRecord
   validates :przedmiot, presence: true, length: { minimum: 5 }
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128141142) do
+ActiveRecord::Schema.define(version: 20180211141917) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "craft_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["craft_id"], name: "index_comments_on_craft_id"
+  end
 
   create_table "crafts", force: :cascade do |t|
     t.string "przedmiot"
